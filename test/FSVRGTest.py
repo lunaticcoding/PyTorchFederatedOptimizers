e64fba0a417f818bca1095ff78eb41e093460192
Marco Papula <ga84div@mytum.de>
2019-07-21 16:50:52 +0200

    #3 fix mistake in test suite

--- test/FSVRGTest.py
+++ test/FSVRGTest.py
@@ -22,13 +22,13 @@ class TestFSVRG(unittest.TestCase):
         self.assertTrue(torch.Tensor.allclose(s, s_correct))
 
     def test_computes_phi(self):
-        phi_ks = torch.Tensor([[1, 1, 1], [.5, .75, 1], [.75, 0, .25],
-                               [.4, .1, 1], [.35, .5, .1], [1.0, 0, .15],
-                               [.1, 0.2, 1], [.5, .75, 0], [.32, 0, .46]
+        phi_ks = torch.Tensor([[[1, 1, 1], [.5, .75, 1], [.75, 0, .25]],
+                               [[.4, .1, 1], [.35, .5, .1], [1.0, 0, .15]],
+                               [[.1, 0.2, 1], [.5, .75, 0], [.32, 0, .46]]
                                ])
         server = Server(DummyNetwork().parameters())
         phi = server.get_phi_and_compute_A(phi_ks)
-        phi_correct = torch.Tensor([[1.5, 1.3, 3], [1.35, 2, 2], [2.07, 0, .86]])
+        phi_correct = torch.Tensor([[1.5, 1.3, 3], [1.35, 2, 1.1], [2.07, 0, .86]])
         self.assertTrue(torch.Tensor.allclose(phi, phi_correct))
 
     def test_computes_A(self):
